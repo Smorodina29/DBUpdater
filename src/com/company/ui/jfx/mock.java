56061d7f6a54a58f5a16d3941ea86e42ce479c95
Дарья <miska156@mail.ru>
2017-03-25 09:49:54 +0300

    [+] added JavaFx dependency and ui template

---
+++ src/com/company/ui/jfx/mock.java
@@ -0,0 +1,24 @@
+package com.company.ui.jfx;/**
+ * Created by Александр on 20.03.2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class mock extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Утилита импорта данных");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+}
