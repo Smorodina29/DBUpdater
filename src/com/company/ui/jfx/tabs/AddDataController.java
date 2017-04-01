99d515d6ca66b1cd3ec6cc3a83a81eca05c8dedd
Дарья <miska156@mail.ru>
2017-04-02 00:04:02 +0300

    [+] expanding results panes

--- src/com/company/ui/jfx/tabs/AddDataController.java
+++ src/com/company/ui/jfx/tabs/AddDataController.java
@@ -1,13 +1,35 @@
 package com.company.ui.jfx.tabs;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TitledPane;
 
 /**
  * Created by Александр on 01.04.2017.
  */
 public class AddDataController {
 
+    public SplitPane root;
+    public TitledPane resultsPane;
+
     public AddDataController() {
+
+    }
+
+    public void initialize() {
+        resultsPane.expandedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean expanded) {
+                if (expanded) {
+                    root.setDividerPositions(0.5);
+                } else {
+                    root.setDividerPositions(0.94);
+                }
+            }
+        });
+        resultsPane.setExpanded(false);
     }
 
     public void upload(ActionEvent actionEvent) {
--- src/com/company/ui/jfx/tabs/UpdateDataController.java
+++ src/com/company/ui/jfx/tabs/UpdateDataController.java
@@ -1,12 +1,34 @@
 package com.company.ui.jfx.tabs;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TitledPane;
 
 /**
  * Created by Александр on 01.04.2017.
  */
 public class UpdateDataController {
+
+    public SplitPane root;
+    public TitledPane resultsPane;
+
     public void upload(ActionEvent actionEvent) {
 
+    }
+
+    public void initialize() {
+        resultsPane.expandedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean expanded) {
+                if (expanded) {
+                    root.setDividerPositions(0.5);
+                } else {
+                    root.setDividerPositions(0.94);
+                }
+            }
+        });
+        resultsPane.setExpanded(false);
     }
 }
